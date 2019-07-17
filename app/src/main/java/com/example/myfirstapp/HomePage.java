0aecf06b47c37875141861183f266ef82cbf9387
Achinths Isuru <achinthaisuru444@gmail.com>
2019-07-17 22:49:29 +0530

    Added a home page Activity

---
+++ app/src/main/java/com/example/myfirstapp/HomePage.java
@@ -0,0 +1,26 @@
+package com.example.myfirstapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class HomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+    public void onClick(View v){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
